bb80cc3602b01b86f3ea5844b5b3cb9be8813f4f
Kajal Thokal <102962520+thokalkajal52@users.noreply.github.com>
2024-12-11 11:10:58 +0530

    Add files via upload
    Python program of to find vowel character.

---
+++ vowelcharacter.py
@@ -0,0 +1,10 @@
+a = input("enter a character :")
+x = len(a)
+if x >1:
+    print("enter a single character :")
+else: 
+    if a =='a' or a == 'e' or a == 'i' or a =='o' or a =='u':
+        print("it is vowel character")
+    else:
+        print("it is not vowel")
+        
